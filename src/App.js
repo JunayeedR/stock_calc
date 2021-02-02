f6c8150bb5c95a785d00075b4ca713920d356844
Junayeed <junayeedr@gmail.com>
2021-02-01 19:18:53 -0500

    Implemented calculations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useRef, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 import Calculate from './Calculate'
+import { v4 as uuidv4 } from 'uuid';
 
 //const LOCAL_STORAGE_KEY = 'stockCalc.stonks'
 
@@ -9,9 +10,9 @@ function App() {
   //{id: 1, price: 12, expected: 15, made: 20}
   const [indiv_stock, setCalc] = useState([])
   
+  const curr_amount_ref = useRef()
   const curr_price_ref = useRef()
-  const curr_expect_ref = useRef()
-  const curr_numb_ref = useRef()
+  const curr_expected_ref = useRef()
 
   // useEffect(() => {
   //   const storedCalc = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
@@ -23,10 +24,19 @@ function App() {
   // }, [stonks])
 
   function handleCalculate(e) {
+    const curr_amount = curr_amount_ref.current.value
     const curr_price = curr_price_ref.current.value
-    console.log(curr_price)
+    const curr_expected = curr_expected_ref.current.value
+    
+    //if (curr_price === '') return 
 
-    curr_price_ref.current.value = null
+    setCalc(prevCalc => {
+      return [... prevCalc, {id: uuidv4(), amount: curr_amount, price: curr_price, expected: curr_expected}]
+    })
+    
+    
+
+    //curr_amount_ref.current.value = null
   }
  
   return (
@@ -35,13 +45,13 @@ function App() {
       <label> Stock Calculator </label> <br />
 
       <label> Amount Invested ($): </label>
-      <input ref = {curr_price_ref} type="text" /> <br />
+      <input ref = {curr_amount_ref} type="text" /> <br />
 
       <label> Stock Price per Share ($): </label>
-      <input ref = {curr_expect_ref} type="text" /> <br />
+      <input ref = {curr_price_ref} type="text" /> <br />
 
       <label> Expected Stocks Value ($): </label>
-      <input ref = {curr_numb_ref} type="text" /> <br />
+      <input ref = {curr_expected_ref} type="text" /> <br />
 
       <button onClick={handleCalculate}> Calculate </button>
 
--- src/Stocks.js
+++ src/Stocks.js
@@ -1,10 +1,23 @@
 import React from 'react'
 
+const format = (num, decimals) => num.toLocaleString('en-US', {
+    minimumFractionDigits: 2,      
+    maximumFractionDigits: 2,
+ });
+
 export default function stocks({curr_stock}) {
     return (
         <div>
             <label> Your </label>
-            {curr_stock.made}
+            {curr_stock.amount / curr_stock.price}
+            <label> shares has increased by $</label>
+            {(curr_stock.expected - curr_stock.price)}
+            <label>, giving a $</label>
+            {(curr_stock.amount / curr_stock.price) * (curr_stock.expected - curr_stock.price)}
+            <label> gain, a </label>
+            {100 * (curr_stock.expected - curr_stock.price) / curr_stock.price}
+            <label>% increase</label><br /><br />
+            
         </div>
     )
 }
